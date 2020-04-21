43ca93197b3457506d0ab5456572f51d735c1797
Orie Steele <orie@transmute.industries>
2020-04-21 15:00:07 -0500

    remove old test suite

--- test-suite/__fixtures__/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  issuers: require("./issuers"),
-  vcVerifiers: require("./vc-verifiers"),
-  vpVerifiers: require("./vp-verifiers"),
-  credentials: require("./credentials"),
-  verifiableCredentials: require("./verifiable-credentials"),
-  verifiablePresentations: require("./verifiable-presentations"),
-  helpers: require("./helpers"),
-};
