0ce2cd992ce6045966454f61438d86ef3a65c06d
Rolson Quadras <rolson.quadras@securekey.com>
2020-04-23 16:47:13 -0400

    chore: TrustBloc plugfest config update (#55)
    - Added a second issuer
    - TrustBloc DID update
    - Name update
    
    Signed-off-by: Rolson Quadras <rolson.quadras@securekey.com>

--- plugfest-2020/vendors/trustbloc/index.js
+++ plugfest-2020/vendors/trustbloc/index.js
@@ -2,7 +2,7 @@ const credentials = require("./credentials");
 const verifiable_credentials = require("./verifiable_credentials");
 const verifiable_presentations = require("./verifiable_presentations");
 module.exports = {
-  name: "Secure Key",
+  name: "SecureKey",
   verify_credential_endpoint:
     "https://verifier.sandbox.trustbloc.dev/verifier/credentials",
   verify_presentation_endpoint:
@@ -12,15 +12,28 @@ module.exports = {
   verifiable_presentations: [...verifiable_presentations],
   issuers: [
     {
-      name: "TrustBloc Issuer",
+      name: "TrustBloc Issuer - TrustBloc DID",
       endpoint:
         "https://issuer.sandbox.trustbloc.dev/vc-issuer-interop/credentials/issueCredential",
       options: [
         {
           issuer:
-            "did:trustbloc:testnet.trustbloc.dev:EiBPf79yB-k9A-7bg3nbaoZCYqRi20X5_Luv4gC1hpbprA==",
+            "did:trustbloc:testnet.trustbloc.dev:EiCK3DGsh6BzGNm4gSTW1UMWHkGk3OrQ52pfTkYH1_DLlg",
           assertionMethod:
-            "did:trustbloc:testnet.trustbloc.dev:EiBPf79yB-k9A-7bg3nbaoZCYqRi20X5_Luv4gC1hpbprA==#key-1",
+            "did:trustbloc:testnet.trustbloc.dev:EiCK3DGsh6BzGNm4gSTW1UMWHkGk3OrQ52pfTkYH1_DLlg#key-1",
+        },
+      ],
+    },
+    {
+      name: "TrustBloc Issuer - DID Key",
+      endpoint:
+          "https://issuer.sandbox.trustbloc.dev/vc-issuer-interop-key/credentials/issueCredential",
+      options: [
+        {
+          issuer:
+              "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
+          assertionMethod:
+              "did:key:z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd#z6MkjRagNiMu91DduvCvgEsqLZDVzrJzFrwahc4tXLt9DoHd",
         },
       ],
     },
