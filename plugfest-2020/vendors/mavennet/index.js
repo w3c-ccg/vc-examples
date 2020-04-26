7aca6e3949aa9414b5555a79d62f9a15bd0d1f02
Mahmoud Alkhraishi <mkhraisha@hotmail.com>
2020-04-26 18:34:05 -0400

    mavennet passing most tests (#64)

--- plugfest-2020/vendors/mavennet/index.js
+++ plugfest-2020/vendors/mavennet/index.js
@@ -1,27 +1,38 @@
-'use strict';
+"use strict";
 
-const credentials = require('./credentials');
-const verifiable_credentials = require('./verifiable_credentials');
-const verifiable_presentations = require('./verifiable_presentations');
+const credentials = require("./credentials");
+const verifiable_credentials = require("./verifiable_credentials");
+const verifiable_presentations = require("./verifiable_presentations");
 
 module.exports = {
-  name: 'Mavennet',
-  verify_credential_endpoint: 'http://api.neo-flow.com/verifier/credentials',
+  name: "Mavennet",
+  verify_credential_endpoint: "https://api.neo-flow.com/verifier/credentials",
   // eslint-disable-next-line max-len
-  verify_presentation_endpoint: 'http://api.neo-flow.com/verifier/presentations',
+  verify_presentation_endpoint:
+    "https://api.neo-flow.com/verifier/presentations",
   credentials: [...credentials],
   verifiable_credentials: [...verifiable_credentials],
   verifiable_presentations: [...verifiable_presentations],
   issuers: [
     {
-      name: 'Neo Flow Issuer',
-      endpoint: 'http://api.neo-flow.com/credentials/issueCredential',
+      name: "Neo Flow Issuer",
+      endpoint: "https://api.neo-flow.com/credentials/issueCredential",
+      // endpoint: "http://localhost:4000/credentials/issueCredential",
       options: [
         {
           // eslint-disable-next-line max-len
-          issuer: 'did:v1:test:nym:z6MkmbrHuhkzwWYeJjkBhTYktabXR22ECzk1WrHJPW69EsJY',
+          issuer: "did:key:z6MkiTsvjrrPNDZ1rrg9QDEYCFWCmEswT6U2cEkScb7edQ9b",
           // eslint-disable-next-line max-len
-          assertionMethod: 'did:v1:test:nym:z6MkmbrHuhkzwWYeJjkBhTYktabXR22ECzk1WrHJPW69EsJY#z6Mkmh3mQN5VNM6yXksw19kiBHLNud8vXSfHE84xtanMchbA',
+          assertionMethod:
+            "did:key:z6MkiTsvjrrPNDZ1rrg9QDEYCFWCmEswT6U2cEkScb7edQ9b#z6MkiTsvjrrPNDZ1rrg9QDEYCFWCmEswT6U2cEkScb7edQ9b",
+        },
+        {
+          // eslint-disable-next-line max-len
+          issuer:
+            "did:v1:test:nym:z6MkfG5HTrBXzsAP8AbayNpG3ZaoyM4PCqNPrdWQRSpHDV6J",
+          // eslint-disable-next-line max-len
+          assertionMethod:
+            "did:v1:test:nym:z6MkfG5HTrBXzsAP8AbayNpG3ZaoyM4PCqNPrdWQRSpHDV6J#z6MkqfvdBsFw4QdGrZrnx7L1EKfY5zh9tT4gumUGsMMEZHY3",
         },
       ],
     },
