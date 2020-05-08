460b8afe1c02ec29defb0ec86d671896760bb64e
kimdhamilton <kimdhamilton@gmail.com>
2020-05-08 13:59:23 -0700

    prettify output json

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ const fs = require('fs');
 const website = "https://w3c-ccg.github.io/vc-examples";
 
 let parent = require('path').resolve(__dirname, '../docs');
-glob(parent + '/**/*.json', {}, (err, files)=>{
+glob(parent + '/**/*.json', {}, (err, files) => {
 
-  var promises = files.map(function(fileName) {
+  var promises = files.map(function (fileName) {
     return fs.promises.readFile(fileName, 'utf-8')
       .then((data) => {
         let jsonData = JSON.parse(data);
         let typeArray = jsonData.type;
         if (typeArray && typeArray.includes("VerifiableCredential")) {
-          typeArray.splice( typeArray.indexOf('VerifiableCredential'), 1 );
+          typeArray.splice(typeArray.indexOf('VerifiableCredential'), 1);
           let fixedName = fileName.substring(parent.length);
           return {
             "fileName": website + fixedName,
             "type": typeArray[0]
           };
-      } else return null;
+        } else return null;
       });
   });
 
@@ -26,10 +26,10 @@ glob(parent + '/**/*.json', {}, (err, files)=>{
     let filtered = results.filter((res) => {
       return res != null;
     });
-    
- 
-    let data = JSON.stringify(filtered);
+
+
+    let data = JSON.stringify(filtered, null, 2);
     fs.writeFileSync(parent + '/index.json', data);
-      });
+  });
 });
 
